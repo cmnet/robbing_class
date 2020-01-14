3076067f375513a50c6326390998740475293419
cmnet <cmnet@yeah.net>
2020-01-14 11:58:35 +0800

    增加老师的操作

--- bll/teacher.py
+++ bll/teacher.py
@@ -1,3 +1,26 @@
 #!/usr/bin/evn python
 # -*- encoding:utf-8 -*-
+from model import *
 __author__ = "CM"
+
+
+@db_session
+def add(t):
+    teacher = Teacher(**t)
+    return teacher
+
+
+@db_session
+def delete(teacher_id):
+    teacher = Teacher.get(id=teacher_id)
+    teacher.delete()
+    commit()
+
+
+@db_session
+def update(teacher):
+    t = Teacher.get(id=teacher["id"])
+    t.name = teacher["name"]
+    t.photo = teacher["photo"]
+    t.brief = teacher["brief"]
+    commit()
