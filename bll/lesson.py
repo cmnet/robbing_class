6acd824e888b2b614116ec32c3b5c63bb681026c
Jelly_yeah <jelly_liu@hotmail.com>
2020-01-16 11:31:22 +0800

    新增了三个操作:1.添加，2.删除，3.更新

--- bll/lesson.py
+++ bll/lesson.py
@@ -1,4 +1,33 @@
 #!/usr/bin/evn python
 # -*- encoding:utf-8 -*-
-__author__ = "CM"
-print("hello world")
+from model import *
+__author__ = "Jelly"
+
+
+@db_session
+def add(lsn):
+    lesson = Lesson(**lsn)
+    return lesson
+
+
+@db_session
+def delete(lesson_id):
+    lesson = Lesson.get(id=lesson_id)
+    lesson.delete()
+    commit()
+
+
+@db_session
+def update(lsn):
+    lesson = Lesson.get(id=lsn["id"])
+    lesson.name = lsn["name"]
+    lesson.teacher = lsn["teacher"]
+    lesson.student_number = lsn["student_number"]
+    lesson.start_time = lsn["start_time"]
+    lesson.address = lsn["address"]
+    lesson.price = lsn["price"]
+    lesson.robbing_start_time = lsn["robbing_start_time"]
+    lesson.robbing_end_time = lsn["robbing_end_time"]
+    lesson.brief = lsn["brief"]
+    lesson.remark = lsn["remark"]
+    commit()
