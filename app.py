6c08b7065f0ab84cc4fc8f842ba1d19e0ee293d7
yourgrandpa123 <2017483534@qq.com>
2020-01-16 12:00:50 +0800

    增加了学生的操作

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from bll import student
 
 app = Flask(__name__)
 
 
 @app.route('/abc')
 def index():
-    return "Hello, world!"
+    return "hello,world!"
 
 
 if __name__ == '__main__':
+    s={
+        "id":"1",
+        "name":"李一一",
+        "mobile":"19982996666",
+        "enroll_year":"2020",
+        "class_name":"编程班"
+    }
+    student.update(s)
+
     app.run()
