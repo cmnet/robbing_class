5302d09f383cc334791096f2b6b041ca83474620
cmnet <cmnet@yeah.net>
2020-01-20 09:46:25 +0800

    fix student

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from bll import student
 from bll import lesson
 app = Flask(__name__)
@@ -6,30 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # lsn = {
-    #     "name": "英语课",
-    #     "teacher": "张老师",
-    #     "student_number": "25",
-    #     "start_time": "2020年3月24日",
-    #     "address": "高升桥路9号918",
-    #     "price": "¥258/2.5小时",
-    #     "robbing_start_time": "2月29日00:00开抢",
-    #     "robbing_end_time": "3月8日12:00截止",
-    #     "brief": "课程难度:★★★★, 授课老师有13余年教学经验, 课堂高效, 趣味学习",
-    #     "remark": "此课程较其他课程稍难, 建议有扎实基础的同学报名"
-    # }
-    # lesson.add(lsn)
-    return "Hello, world!"
+    return "hello, world"
+
+
+@app.route("/student/list")
+def student_list():
+    students = student.query()
+    return render_template("student_list.html", data=students)
 
 
 if __name__ == '__main__':
-    s={
-        "id":"1",
-        "name":"王五",
-        "mobile":"19982996666",
-        "enroll_year":"2019",
-        "class_name":"小猫编程"
-    }
-    student.update(s)
-
     app.run()
